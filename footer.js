0a946bacdcb18a0212e24fd473326f56f86769fb
SelftaughtVN <138423736+SelftaughtVN@users.noreply.github.com>
2023-07-20 12:08:11 -0700

    Update footer.js
    Signed-off-by: SelftaughtVN <138423736+SelftaughtVN@users.noreply.github.com>

--- footer.js
+++ footer.js
@@ -4,35 +4,26 @@ class rfooter extends HTMLElement {
     }
     connectedCallback() {
       this.innerHTML= `<style>
-        footer > .container {
-            margin-top: 80px;
-            margin-bottom: 80px;
-        }
         footer {
             background: #01faff;
             background: radial-gradient(at center, #01faff, #00b2e9);
-            padding-top: 5px;
-            padding-bottom: 5px;
-            margin-top: 40px;
             text-align: left;
+            padding: 80px 0 80px 0;
         }
         .footnote {
             font-family: Arvo, serif;
             font-size: 0.875em;
             font-weight: 400;
             color: white;
-            margin-bottom: 0;
-            padding-left: 20px;
         }
         a.email {
             color: white;
             text-decoration: underline white solid auto;
         }
         </style>
-        <footer class="py-1 px-0">
+        <footer>
             <div class="container">
                 <p class="footnote">Asking a question? Email <a class='email' href="mailto:rylex.phan@gmail.com">rylex.phan@gmail.com</a></p>
-                <p></p>
                 <p class="footnote">We wish you a supercalifragilisticexpialidocious day!</p>
             </div>
         </footer>`
