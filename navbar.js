248250661121b68da96a865694e4c692b72dec1a
SelftaughtVN <138423736+SelftaughtVN@users.noreply.github.com>
2023-08-01 14:46:00 -0700

    Update navbar.js
    Signed-off-by: SelftaughtVN <138423736+SelftaughtVN@users.noreply.github.com>

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     var collapsible = document.querySelector(".navbar-collapse");
     var navlinks = document.querySelectorAll(".nav-link");
     var navbar = document.querySelector(".navbar");
@@ -7,47 +7,47 @@ document.addEventListener("DOMContentLoaded", function () {
     var icon = document.querySelector(".navbar-toggler-icon");
     function resetNavLinks() {
         navlinks.forEach((elem) => {
-        elem.style.color = "";
-        elem.style.setProperty("--underline-color", "255,255,255");
+            elem.style.color = "";
+            elem.style.setProperty("--underline-color", "255,255,255");
         });
     }
     function handleResize() {
         if (window.innerWidth > 767 && window.scrollY == 0) {
-        resetNavLinks();
-        collapsible.style.backgroundColor = "transparent";
-        collapsible.classList.remove("show");
-        }
-    }
-    function handleCollapse() {
-        if (window.innerWidth < 768) {
-        navlinks.forEach((elem) => {
-            elem.style.color = "black";
-            elem.style.setProperty("--underline-color", "0,0,0");
-        });
-        collapsible.style.backgroundColor = "#efefef";
-        }
-    }
-    function handleScroll() {
-        if (window.scrollY > 0) {
-        navlinks.forEach((elem) => {
-            elem.style.color = "black";
-            elem.style.setProperty("--underline-color", "0,0,0");
-        });
-        navbar.classList.add("scroll");
-        brandname.style.color = "black";
-        iconborder.style.borderColor = "black";
-        icon.style.backgroundImage =
-            "url(\"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgb(0,0,0)' stroke-width='2' stroke-linecap='round' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E\")";
-        } else {
-        navbar.classList.remove("scroll");
-        brandname.style.color = "";
-        iconborder.style.borderColor = "";
-        icon.style.backgroundImage = "";
-        if (!collapsible.classList.contains("show") || window.innerWidth > 767) {
             resetNavLinks();
             collapsible.style.backgroundColor = "transparent";
             collapsible.classList.remove("show");
         }
+    }
+    function handleCollapse() {
+        if (window.innerWidth < 768) {
+            navlinks.forEach((elem) => {
+                elem.style.color = "black";
+                elem.style.setProperty("--underline-color", "0,0,0");
+            });
+            collapsible.style.backgroundColor = "#efefef";
+        }
+    }
+    function handleScroll() {
+        if (window.scrollY > 0) {
+            navlinks.forEach((elem) => {
+                elem.style.color = "black";
+                elem.style.setProperty("--underline-color", "0,0,0");
+            });
+            navbar.classList.add("scroll");
+            brandname.style.color = "black";
+            iconborder.style.borderColor = "black";
+            icon.style.backgroundImage =
+                "url(\"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgb(0,0,0)' stroke-width='2' stroke-linecap='round' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E\")";
+        } else {
+            navbar.classList.remove("scroll");
+            brandname.style.color = "";
+            iconborder.style.borderColor = "";
+            icon.style.backgroundImage = "";
+            if (!collapsible.classList.contains("show") || window.innerWidth > 767) {
+                resetNavLinks();
+                collapsible.style.backgroundColor = "transparent";
+                collapsible.classList.remove("show");
+            }
         }
     }
     window.addEventListener("resize", handleResize);
@@ -81,7 +81,7 @@ class rNavbar extends HTMLElement {
             li.nav-item {
                 all: initial;
             }
-            a.nav-link {
+            a.nav-link, button.nav-link {
                 font-family: Comfortaa, cursive;
                 font-size: 0.9em;
                 --underline-color: 255, 255, 255;
@@ -90,7 +90,7 @@ class rNavbar extends HTMLElement {
                 text-underline-offset: 10px;
                 transition: text-decoration-color 300ms;
             }
-            a.nav-link:hover {
+            a.nav-link:hover, button.nav-link:hover {
                 color: white;
                 text-decoration-color: rgba(var(--underline-color), 1);
             }
@@ -113,6 +113,8 @@ class rNavbar extends HTMLElement {
             }
             div.collapse.navbar-collapse {
                 background-color: transparent;
+                padding-bottom: 7.5px;
+                padding-top: 6.5px;
             }
         </style>
         <nav class="navbar fixed-top navbar-expand-md transparent py-0">
